cc32f7c3d42f00829f85079d866c0c9bef8862a6
Julien Tellier <telliouze@MacBook-Pro-de-Julien.local>
2016-11-14 16:26:53 +0100

    Ajout du squelette de l'appli et du fichier JSon

---
+++ SeriesTV/SeriesTV/src/com/gogojuju/series/Acteur.java
@@ -0,0 +1,12 @@
+package com.gogojuju.series;
+
+public class Acteur {
+
+	private String nom;
+	private String prenom;
+	
+	public Acteur(String n, String p){
+		this.nom = n;
+		this.prenom = p;
+	}
+}
